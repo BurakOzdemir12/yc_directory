75b0401e71ada846ec20187dc0adfd30a08bce0b
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2025-06-24 17:26:15 +0300

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({searchParams}:
+                                   {
+                                       searchParams: Promise<{ query?: string }>
+                                   }) {
+    const query = (await searchParams).query;
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: {_id: 1, name:'Elon Musk'},
+        _id: 1,
+        description: 'Lorem ipsum dolor sit amet, consectetur ',
+        image: "https://picsum.photos/id/1/350/150",
+        category: 'Robots',
+        title: "We Robots"
+
+    }]
+    return (
+        < >
+            <section className="pink_container pattern">
+                <h1 className="heading">Pitch your startup, <br/> connect with entrepreneurs</h1>
+                <p className="sub-heading max-w-3xl  ">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+                    Competitions </p>
+                <SearchForm query={query}/>
+            </section>
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : 'All Startups'}
+                </p>
+                <ul className="card_grid">
+                    {posts ?.length > 0 ? (
+                        posts.map((post)=>(
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ):
+                    <p className="no-results ">
+                        No startups found.
+                    </p>
+                }
+                </ul>
+            </section>
+        </>
+    )
+}
+
+
